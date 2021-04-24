df39106026d8be5b8184e00329b9a1bb2c726ac0
Pratham <pratham071@gmail.com>
2021-04-25 01:45:52 +0530

    Heroku deployment error testing

--- booklib/accounts/models.py
+++ booklib/accounts/models.py
@@ -2,17 +2,17 @@ from django.db import models
 from django.contrib.auth.models import Group, Permission
 
 # Create your models here.
-Librarian, created = Group.objects.get_or_create(name='Librarian')
+# Librarian, created = Group.objects.get_or_create(name='Librarian')
 
-MODELS = ['book', 'book instance', 'author', 'requested book']
-PERMISSIONS = ['view', 'add', 'delete', 'change']
-for model in MODELS:
-    for permission in PERMISSIONS:
-        name = f'Can {permission} {model}'
+# MODELS = ['book', 'book instance', 'author', 'requested book']
+# PERMISSIONS = ['view', 'add', 'delete', 'change']
+# for model in MODELS:
+#     for permission in PERMISSIONS:
+#         name = f'Can {permission} {model}'
 
-        try:
-            model_add_perm = Permission.objects.get(name=name)
-        except Permission.DoesNotExist:
-            continue
+#         try:
+#             model_add_perm = Permission.objects.get(name=name)
+#         except Permission.DoesNotExist:
+#             continue
 
-        Librarian.permissions.add(model_add_perm)
+#         Librarian.permissions.add(model_add_perm)
