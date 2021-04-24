a129adfca27afd530acc010018e4541048f3aa1c
Pratham <pratham071@gmail.com>
2021-04-25 01:19:49 +0530

    Updated settings.py for setup on Heroku

--- booklib/booklib/settings.py
+++ booklib/booklib/settings.py
@@ -24,12 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$q1f!a7o-ca)$2@&70w^5ia-qa)$e0aho*h0bss%)w9bn#1%*d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://polar-chamber-13872.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
